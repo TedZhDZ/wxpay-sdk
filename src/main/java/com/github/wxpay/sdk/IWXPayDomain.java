6e41fb7ddcecdd1f16ec63d566b2980932679033
dezhi_zhang <wqwq321@qq.com>
2018-08-01 11:14:46 +0800

    修复WXPayConfig中abtract方法的访问权限

--- src/main/java/com/github/wxpay/sdk/IWXPayDomain.java
+++ src/main/java/com/github/wxpay/sdk/IWXPayDomain.java
@@ -3,7 +3,7 @@ package com.github.wxpay.sdk;
 /**
  * 域名管理，实现主备域名自动切换
  */
-public abstract interface IWXPayDomain {
+public interface IWXPayDomain {
     /**
      * 上报域名网络状况
      * @param domain 域名。 比如：api.mch.weixin.qq.com
@@ -13,14 +13,14 @@ public abstract interface IWXPayDomain {
      *           ConnectTimeoutException，表示建立网络连接异常
      *           UnknownHostException， 表示dns解析异常
      */
-    abstract void report(final String domain, long elapsedTimeMillis, final Exception ex);
+    void report(final String domain, long elapsedTimeMillis, final Exception ex);
 
     /**
      * 获取域名
      * @param config 配置
      * @return 域名
      */
-    abstract DomainInfo getDomain(final WXPayConfig config);
+    DomainInfo getDomain(final WXPayConfig config);
 
     static class DomainInfo{
         public String domain;       //域名
--- src/main/java/com/github/wxpay/sdk/WXPayConfig.java
+++ src/main/java/com/github/wxpay/sdk/WXPayConfig.java
@@ -4,14 +4,12 @@ import java.io.InputStream;
 
 public abstract class WXPayConfig {
 
-
-
     /**
      * 获取 App ID
      *
      * @return App ID
      */
-    abstract String getAppID();
+	protected abstract String getAppID();
 
 
     /**
@@ -19,7 +17,7 @@ public abstract class WXPayConfig {
      *
      * @return Mch ID
      */
-    abstract String getMchID();
+	protected abstract String getMchID();
 
 
     /**
@@ -27,7 +25,7 @@ public abstract class WXPayConfig {
      *
      * @return API密钥
      */
-    abstract String getKey();
+	protected abstract String getKey();
 
 
     /**
@@ -35,7 +33,7 @@ public abstract class WXPayConfig {
      *
      * @return 商户证书内容
      */
-    abstract InputStream getCertStream();
+	protected abstract InputStream getCertStream();
 
     /**
      * HTTP(S) 连接超时时间，单位毫秒
@@ -59,7 +57,7 @@ public abstract class WXPayConfig {
      * 获取WXPayDomain, 用于多域名容灾自动切换
      * @return
      */
-    abstract IWXPayDomain getWXPayDomain();
+    protected abstract IWXPayDomain getWXPayDomain();
 
     /**
      * 是否自动上报。
